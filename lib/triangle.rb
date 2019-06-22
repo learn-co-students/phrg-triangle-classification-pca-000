5ab4282752fc1caae2051fb76cf70081486fe662
gillianwenhold <gwenhold@mac.com>
2019-06-21 21:40:34 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
-  # write code here
+
+  attr_accessor :s1, :s2, :s3
+
+  def initialize(s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+  end
+
+  def kind
+    if @s1 + @s2 > @s3 && @s2 + @s3 > @s1 && @s3 + @s1 > @s2
+      if @s1 == @s2 && @s2 == @s3
+        :equilateral
+      elsif @s1 == @s2 || @s2 == @s3 || @s3 == @s1
+        :isosceles
+      else
+        :scalene
+      end
+    else
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      puts "This is not a valid triangle. Please make sure each side is larger than 0, and the sum of any two sides is greater than the third."
+    end
+  end
+
 end
